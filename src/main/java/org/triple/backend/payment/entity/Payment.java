d0a1009f77258630a3d95bf5a32fa01a20406bc2
sangyunpark99 <psangyun5108@gmail.com>
2026-02-05 15:17:29 +0900

    [#1] feat: PG사 연동 Payment 엔티티 구현

---
+++ src/main/java/org/triple/backend/payment/entity/Payment.java
@@ -0,0 +1,45 @@
+package org.triple.backend.payment.entity;
+
+import jakarta.persistence.*;
+import org.triple.backend.global.common.BaseEntity;
+import org.triple.backend.invoice.entity.Invoice;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Payment extends BaseEntity {
+
+    @Id
+    @Column(name = "payment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "invoice_id")
+    private Invoice invoice;
+
+    private String pgProvider;
+
+    private String method;
+
+    private String orderId;
+
+    private String paymentKey;
+
+    private BigDecimal requestedAmount;
+
+    private BigDecimal approvedAmount;
+
+    private PaymentStatus paymentStatus;
+
+    private LocalDateTime approvedAt;
+
+    private LocalDateTime requestedAt;
+
+    private String receiptUrl;
+
+    private String failureCode;
+
+    private String failureMessage;
+}

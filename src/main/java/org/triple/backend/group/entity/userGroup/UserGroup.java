a72703202adb996e01664662f444eb494ce0ba5b
sangyunpark99 <psangyun5108@gmail.com>
2026-02-05 15:18:27 +0900

    [#1] feat: 사용자와 그룹의 중간 테이블 엔티티 구현

---
+++ src/main/java/org/triple/backend/group/entity/userGroup/UserGroup.java
@@ -0,0 +1,35 @@
+package org.triple.backend.group.entity.userGroup;
+
+import jakarta.persistence.*;
+import org.triple.backend.global.common.BaseEntity;
+import org.triple.backend.group.entity.group.Group;
+import org.triple.backend.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class UserGroup extends BaseEntity{
+
+    @Id
+    @Column(name = "user_group_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Group group;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private JoinStatus joinStatus;
+
+    private LocalDateTime joinedAt;
+
+    private LocalDateTime leftAt;
+}

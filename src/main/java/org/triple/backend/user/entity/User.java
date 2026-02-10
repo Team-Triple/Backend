24f6cc878b4a463850240dca523968da7dbf096d
sangyunpark99 <psangyun5108@gmail.com>
2026-02-10 17:55:31 +0900

    [#9] refactor: LocalDateTime에서 LocalDate로 변경

--- src/main/java/org/triple/backend/user/entity/User.java
+++ src/main/java/org/triple/backend/user/entity/User.java
@@ -1,17 +1,26 @@
 package org.triple.backend.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.triple.backend.global.common.BaseEntity;
 import org.triple.backend.group.entity.joinApply.JoinApply;
 import org.triple.backend.group.entity.userGroup.UserGroup;
 import org.triple.backend.invoice.entity.InvoiceUser;
 import org.triple.backend.travel.entity.UserTravelItinerary;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User extends BaseEntity {
 
     @Id
@@ -26,21 +35,25 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    private LocalDateTime birth;
+    private LocalDate birth;
 
     private String description;
 
     private String profileUrl;
 
     @OneToMany(mappedBy = "user")
+    @Builder.Default
     private List<JoinApply> joinApplies = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
+    @Builder.Default
     private List<UserGroup> userGroups = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
+    @Builder.Default
     private List<UserTravelItinerary> userTravelItineraries = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
+    @Builder.Default
     private List<InvoiceUser> invoiceUsers = new ArrayList<>();
 }

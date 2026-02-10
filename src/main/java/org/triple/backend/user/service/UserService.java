118a38db2cfd183a6a8e59641f07dd20251a7004
sangyunpark99 <psangyun5108@gmail.com>
2026-02-10 17:57:00 +0900

    [#9] feat: 내 정보 조회 API 비즈니스 로직 구현

---
+++ src/main/java/org/triple/backend/user/service/UserService.java
@@ -0,0 +1,33 @@
+package org.triple.backend.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.triple.backend.global.error.BusinessException;
+import org.triple.backend.user.dto.response.UserInfoResponseDto;
+import org.triple.backend.user.entity.User;
+import org.triple.backend.user.exception.UserErrorCode;
+import org.triple.backend.user.repository.UserJpaRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserJpaRepository userJpaRepository;
+
+    @Transactional(readOnly = true)
+    public UserInfoResponseDto userInfo(final Long userId) {
+
+        User savedUser = userJpaRepository.findById(userId).orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+
+        return UserInfoResponseDto.builder()
+                .userId(savedUser.getId())
+                .nickname(savedUser.getNickname())
+                .gender(savedUser.getGender().toString())
+                .birth(savedUser.getBirth())
+                .description(savedUser.getDescription())
+                .profileUrl(savedUser.getProfileUrl())
+                .build();
+    }
+
+}

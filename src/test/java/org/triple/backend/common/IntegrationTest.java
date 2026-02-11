d5e202c9d0d8c390a3c2018089c16d8374cd9d3d
sangyunpark99 <psangyun5108@gmail.com>
2026-02-11 09:54:05 +0900

    [#9] test: 통합 테스트 공통 사용 코드 추상화

---
+++ src/test/java/org/triple/backend/common/IntegrationTest.java
@@ -0,0 +1,16 @@
+package org.triple.backend.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class IntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+}

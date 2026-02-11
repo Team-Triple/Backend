bb166450b4e10c43f4f27b2507252b9ed75ba586
sangyunpark99 <psangyun5108@gmail.com>
2026-02-11 13:39:18 +0900

    [#9] refactor: 사용하지 않는 클래스 제거

--- src/test/java/org/triple/backend/common/RepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package org.triple.backend.common;
-
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@DataJpaTest
-@ActiveProfiles("test")
-public class RepositoryTest {
-}

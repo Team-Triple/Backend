25cf828b95006c60da5ae4ed8b12bcca34697544
sangyunpark99 <psangyun5108@gmail.com>
2026-02-11 13:38:34 +0900

    [#9] refactor: ControllerTest 공통 어노테이션 @WebMvcDocsTest 어노테이션으로 추상화
    - 클래스랑 이름이 겹쳐서 @WebMvcTest로 선언

--- src/test/java/org/triple/backend/common/ControllerTest.java
+++ src/test/java/org/triple/backend/common/ControllerTest.java
@@ -1,32 +1,21 @@
 package org.triple.backend.common;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.triple.backend.user.controller.UserController;
-import org.triple.backend.user.service.UserService;
+import org.triple.backend.common.annotation.WebMvcDocsTest;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
-@WebMvcTest(UserController.class)
-@ExtendWith(RestDocumentationExtension.class)
-@ActiveProfiles("test")
-public abstract class ControllerTest {
+@WebMvcDocsTest
+public class ControllerTest {
 
     @Autowired
     protected MockMvc mockMvc;
-
-    @MockitoBean
-    protected UserService userService;
 
     @BeforeEach
     void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {

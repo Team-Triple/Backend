7b756851df23d1e6d575cf9efcbd53dffe18940c
sangyunpark99 <psangyun5108@gmail.com>
2026-02-13 18:03:00 +0900

    [#20] feat: 그룹 생성 Controller 구현

--- src/main/java/org/triple/backend/group/controller/GroupController.java
+++ src/main/java/org/triple/backend/group/controller/GroupController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.triple.backend.auth.session.LoginRequired;
+import org.triple.backend.auth.session.LoginUser;
 import org.triple.backend.group.dto.request.CreateGroupRequestDto;
 import org.triple.backend.group.dto.response.CreateGroupResponseDto;
 import org.triple.backend.group.service.GroupService;
@@ -17,8 +19,9 @@ public class GroupController {
 
     private final GroupService groupService;
 
+    @LoginRequired
     @PostMapping
-    public CreateGroupResponseDto create(@Valid @RequestBody CreateGroupRequestDto createGroupRequestDto) {
-        return groupService.create(createGroupRequestDto);
+    public CreateGroupResponseDto create(@Valid @RequestBody CreateGroupRequestDto createGroupRequestDto, @LoginUser final Long userId) {
+        return groupService.create(createGroupRequestDto, userId);
     }
 }

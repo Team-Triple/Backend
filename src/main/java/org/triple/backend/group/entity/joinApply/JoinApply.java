b19728023a5895299cd838bcbc0d4a4de1f99e4e
sangyunpark99 <psangyun5108@gmail.com>
2026-02-19 15:12:29 +0900

    [#56] feat: JoinApply 엔티티 Builder 패턴 추가

--- src/main/java/org/triple/backend/group/entity/joinApply/JoinApply.java
+++ src/main/java/org/triple/backend/group/entity/joinApply/JoinApply.java
@@ -1,11 +1,17 @@
 package org.triple.backend.group.entity.joinApply;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.triple.backend.global.common.BaseEntity;
 import org.triple.backend.group.entity.group.Group;
 import org.triple.backend.user.entity.User;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JoinApply extends BaseEntity {
 
     @Id

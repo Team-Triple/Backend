90e9ed084c9c82f98834d55b9e0a5a8a591e7995
sangyunpark99 <psangyun5108@gmail.com>
2026-02-19 18:43:48 +0900

    [#56] feat: 가입 신청 내역 bulk delete 구현, groupId, userId, role 기반으로 UserGroup 엔티티 존재 여부 조회 로직 구현

--- src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
@@ -1,7 +1,16 @@
 package org.triple.backend.group.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.triple.backend.group.entity.userGroup.Role;
 import org.triple.backend.group.entity.userGroup.UserGroup;
 
 public interface UserGroupJpaRepository extends JpaRepository<UserGroup, Long> {
+
+    boolean existsByGroupIdAndUserIdAndRole(Long groupId, Long userId, Role role);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM UserGroup ug WHERE ug.group.id = :groupId")
+    void bulkDeleteByGroupId(Long groupId);
 }
